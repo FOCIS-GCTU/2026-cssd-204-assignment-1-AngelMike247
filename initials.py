cde6ca5a580db536424709f4cf935a200757e163
AngelMike247 <2425404029@live.gctu.edu.gh>
2026-05-30 12:28:44 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Akpai Michael Delali
+# STUDENT ID: 2425404029
+# Email: akpaiangelmichael@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Akpai Michael Delali, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+    # Print my initials in large stylized letters.
+    print()
+    print("...AMD...")
+    print()
+
+    print("...AAAAAAAAAAAA****...MMMM......MMMM****...DDDDDDDDDDDD****")
+    print("...AAAA....AAAA****...MMMMM....MMMMM****...DDDD....DDDD****")
+    print("...AAAA....AAAA****...MMMMMM..MMMMMM****...DDDD.....DDD****")
+    print("...AAAA....AAAA****...MM..MMMMMM..MM****...DDDD.....DDD****")
+    print("...AAAAAAAAAAAA****...MM...MMMM...MM****...DDDD.....DDD****")
+    print("...AAAAAAAAAAAA****...MM....MM....MM****...DDDD.....DDD****")
+    print("...AAAA....AAAA****...MM..........MM****...DDDD.....DDD****")
+    print("...AAAA....AAAA****...MM..........MM****...DDDD....DDDD****")
+    print("...AAAA....AAAA****...MM..........MM****...DDDDDDDDDDDD****")
+    print("...AAAA....AAAA****...MM..........MM****...DDDDDDDDDDDD****")
+
+    print()
 
 
 main()
+
